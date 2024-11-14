4c993e288ab6422d0aac8434ed9c25270266b007
Christian M <christianescobarmarquez@gmail.com>
2024-11-14 14:53:33 -0300

    cambios generales y añado cosas a colecciones

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const menuToggle = document.querySelector('.menu-toggle');
 const navContainer = document.querySelector('.nav-container');
 const socialIcons = document.querySelector('.social-icons2');
@@ -7,18 +6,17 @@ const coleccionesLink = document.querySelector('.colecciones-link');
 const menu = document.querySelector('.colecciones-content');
 let isMenuOpen = false;
 
-
 document.addEventListener('DOMContentLoaded', () => {
-    menuToggle.addEventListener('click', () => {
-        navContainer.classList.toggle('show'); // Alternar la clase 'show' para mostrar/ocultar el menú
-        socialIcons.classList.toggle('show'); // Alternar la visibilidad de los íconos de redes sociales
-        if (navContainer.classList.contains('show')) {
-          headerTitle.style.opacity = '1'; // Muestra el header-title al abrir
-        } else {
-          headerTitle.style.opacity = '0'; // Oculta el header-title al cerrar
-        }
-      });
-    // Ocultar los íconos de redes sociales cuando se hace clic fuera del menú
+  menuToggle.addEventListener('click', () => {
+    navContainer.classList.toggle('show'); // Alternar la clase 'show' para mostrar/ocultar el menú
+    socialIcons.classList.toggle('show'); // Alternar la visibilidad de los íconos de redes sociales
+    if (navContainer.classList.contains('show')) {
+      headerTitle.style.opacity = '1'; // Muestra el header-title al abrir
+    } else {
+      headerTitle.style.opacity = '0'; // Oculta el header-title al cerrar
+    }
+  });
+  // Ocultar los íconos de redes sociales cuando se hace clic fuera del menú
   document.addEventListener('click', (event) => {
     if (
       !menuToggle.contains(event.target) &&
@@ -65,13 +63,20 @@ document.addEventListener('DOMContentLoaded', () => {
       isMenuOpen = true;
 
       // Redirigir a la página de colecciones
-      const redirectPageUrl = 'https://josefinaorrego.cl/pages/colecciones.html'; // Cambia esto a la URL de la otra página
+      const redirectPageUrl =
+        'https://josefinaorrego.cl/pages/colecciones.html'; // Cambia esto a la URL de la otra página
       window.location.href = redirectPageUrl; // Redirigir a la página de colecciones
     }
   });
-  // Cerrar el menú si se hace clic fuera de él
+  // Cerrar el menú si se hace clic fuera de él solo en vista móvil
   document.addEventListener('click', function (event) {
-    if (!menu.contains(event.target) && event.target !== coleccionesLink) {
+    const isMobile = window.innerWidth <= 768; // Ajusta este valor según tus necesidades
+
+    if (
+      isMobile &&
+      !menu.contains(event.target) &&
+      event.target !== coleccionesLink
+    ) {
       menu.style.display = 'none';
       isMenuOpen = false;
     }
@@ -89,13 +94,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Detectar elementos al hacer scroll
 const observador = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-      }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
   });
-  
-  // Observar todos los elementos con la clase fade-in
-  document.querySelectorAll('.fade-in').forEach((el) => observador.observe(el));
-  
+});
+
+// Observar todos los elementos con la clase fade-in
+document.querySelectorAll('.fade-in').forEach((el) => observador.observe(el));
